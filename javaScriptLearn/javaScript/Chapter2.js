979118bd1b4bdb1af8ededa22b4c2300b654ed3d
Pawel <pawlo57@o2.pl>
2013-05-09 23:38:59 +0200

    Chapter 2 ended

---
+++ javaScriptLearn/javaScript/Chapter2.js
@@ -0,0 +1,31 @@
+﻿//2.7 trim strings
+function trimWhiteSpace() {
+    var testString = "    this is the string    ";
+
+    //trim white space from the begining, deleting one or more saces from begining of string
+    testString = testString.replace(/^\s+/, "");
+
+    //trim white space from the end
+    testString = testString.replace(/\s+$/, "");
+
+    var testString = "    this is the string     ";
+
+    testString = testString.trim();
+
+    document.getElementById("result").innerHTML = testString;
+}
+
+//2.8 Replace HTML tags with Named Entities
+function replaceHtmlTags() {
+    var pieceOdHtml = "<p>This is a <span>paragraph</span></p>";
+
+    pieceOdHtml = pieceOdHtml.replace(/</g, "&lt;");
+    pieceOdHtml - pieceOdHtml.replace(/>/g, "&gt;");
+
+    document.getElementById("result").innerHTML = pieceOdHtml;
+}
+
+window.onload = function onLoad() {
+    trimWhiteSpace();
+    replaceHtmlTags();
+}
