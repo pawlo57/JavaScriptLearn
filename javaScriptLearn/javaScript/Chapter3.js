65ca972acfa785e771f8cdb591ca98ef7024e362
Pawel <pawlo57@o2.pl>
2013-05-10 23:10:56 +0200

    3.3 Printig out an ISO 8601 Formatted date

--- javaScriptLearn/javaScript/Chapter3.js
+++ javaScriptLearn/javaScript/Chapter3.js
@@ -1,16 +1,47 @@
-﻿window.onload = function () {
-    getTodaysDate();
-};
-
-//3.1Print today's date
+﻿//3.1Print today's date
 function getTodaysDate() {
-    var dateElement = document.getElementById("date");
+    //var dateElement = document.getElementById("date");
     var date = new Date();
 
-    var result = "<p>" + dt + "</p>";
-    insertResult(result);
+    insertResult(date);
+}
+
+//3.2 Printing out th UTC Date and Time
+function getUTCDateAndTime() {
+    var today = new Date();
+
+    var utcDate = today.toUTCString();
+    var dateElement = "local datetime: " + today + " UTC datetime: " + utcDate;
+    insertResult(dateElement);
+}
+
+//3.3 Printig out an ISO 8601 Formatted date
+function getISO8601Date() {
+    var date = new Date();
+    var month = date.getUTCMonth();
+    month++;
+
+    var day = date.getUTCDate();
+    if (day < 10) day = "0" + day;
+    var year = date.getUTCFullYear();
+
+    var hours = date.getUTCHours();
+    if (hours < 10) hours = "0" + hours;
+
+    var minutes = date.getUTCMinutes();
+    if (minutes < 10) minutes = "0" + minutes;
+
+    var seconds = date.getUTCSeconds();
+    if (seconds < 10) seconds = "0" + seconds;
+
+    var newDate = year + "-" + month + "-" + day + "T" + hours + ":" + minutes + ":" + seconds + "Z";
 }
 
 function insertResult(result) {
-    document.getElementById("result").innerHTML = "dd";
+    document.getElementById("result").innerHTML += "<p>" + result; + "</p>";
+}
+
+window.onload = function onLoad() {
+    getTodaysDate();
+    getUTCDateAndTime();
 }
