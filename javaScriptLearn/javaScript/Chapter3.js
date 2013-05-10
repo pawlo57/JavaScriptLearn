b86bc7b2026269e06cff9ee59c35e97dadce6630
Pawel <pawlo57@o2.pl>
2013-05-10 22:08:28 +0200

    3.1-Print todays date

---
+++ javaScriptLearn/javaScript/Chapter3.js
@@ -0,0 +1,16 @@
+﻿window.onload = function () {
+    getTodaysDate();
+};
+
+//3.1Print today's date
+function getTodaysDate() {
+    var dateElement = document.getElementById("date");
+    var date = new Date();
+
+    var result = "<p>" + dt + "</p>";
+    insertResult(result);
+}
+
+function insertResult(result) {
+    document.getElementById("result").innerHTML = "dd";
+}
